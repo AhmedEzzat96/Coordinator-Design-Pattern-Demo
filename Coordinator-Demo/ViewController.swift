96a94bf3c6487c599bb0fad1ade956121f32c569
Ahmed Ezzat <ahmed.ezzat2410@gmail.com>
2020-11-25 22:25:52 +0200

    Initial Commit

---
+++ Coordinator-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinator-Demo
+//
+//  Created by Ahmed Ezzat on 25/11/2020.
+//  Copyright © 2020 IDEAcademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
